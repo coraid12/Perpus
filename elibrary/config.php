fb63c553df2397245e503ec188e0c6055df3efb3
secreal <curious_kidimoet@yahoo.com>
2022-12-02 12:04:21 +0700

    fix: ganti database

--- elibrary/config.php
+++ elibrary/config.php
@@ -84,12 +84,12 @@ define("DEFAULT_EMAIL", "");
 define("DEFAULT_EMAIL_ACCOUNT_NAME", "");
 
 // Database Configuration Settings
-define("DB_HOST", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "perpus");
+define("DB_HOST", "elibrary.ngantrionline.com");
+define("DB_USERNAME", "elibrary");
+define("DB_PASSWORD", "elibraryperpus");
+define("DB_NAME", "elibrary");
 define("DB_TYPE", "mysql");
-define("DB_PORT", "");
+define("DB_PORT", "33060");
 define("DB_CHARSET", "utf8");
 
 define("MAX_RECORD_COUNT", 20); //Default Max Records to Retrieve  per Page
